f3ca5a0449b0e7ee04fa1c48dd84d2189f8cd749
Alexander1248 <sasha.izmyloff@mail.ru>
2022-07-28 15:01:40 +0300

    v1.1.1 Tokenizer interface

---
+++ src/main/java/ru/alexander1248/nnlib/shell/tools/tokenizer/Tokenizer.java
@@ -0,0 +1,8 @@
+package ru.alexander1248.nnlib.shell.tools.tokenizer;
+
+import java.util.List;
+
+public interface Tokenizer {
+    List<float[]> tokenize(String data);
+    String untokenize(List<float[]> data);
+}
